d4042831f7a5b6ff5e3b1d68537fff5b84d44944
Parthiban024 <parthiban@objectways.com>
2023-07-04 14:36:13 +0530

    new

--- server.js
+++ server.js
@@ -1,86 +1,429 @@
 const express = require('express');
-const multer = require('multer');
-const nodemailer = require('nodemailer');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const nodemailer = require("nodemailer");
+const jwt = require('jsonwebtoken');
+const router = require('./backend/routes/router');
 const path = require('path')
+const Review = require('./backend/models/review');
+const ReviewTwo = require('./backend/models/timechamp');
+const ReviewThree = require('./backend/models/hrreview');
+require('dotenv').config(); // load environment variables from .env file
+
+
+
+
+let reviews = [];
+let timechamp = [];
+let hrreview = [];
+
 
 const app = express();
-app.use(express.json());
-app.use(cors()); // Enable CORS
+app.use(cors());
+// setup body-parser middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Multer setup for file uploads
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
-const upload = multer({ storage });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('MongoDB connected');
+});
 
-// API endpoint for job application form submission
-app.post('/api/apply-job', upload.single('resume'), (req, res) => {
+// Routes
+app.use('/api', router);
+
+
+// Add a new review
+app.post('/api/reviews', (req, res) => {
+
+  const { employeeName, employeeId, emailId, systemNo, systemType, systemTypetwo, unitNo, floorNo, teamName, teamManager, priority, issueDate, description } = req.body;
+
+  const newReview = new Review({
+employeeName, employeeId, emailId, systemNo, systemType, systemTypetwo, unitNo, floorNo, teamName, teamManager, priority, issueDate, description
+  });
+
+  newReview.save()
+    .then(review => {
+      res.send(review);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Error saving review');
+    });
+
+  // nodemailer function
+  const transpoter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'parthibaneee7548@gmail.com',
+      pass: 'xnzrszhaawvpkcov'
+    }
+  })
+  const mailOptions = {
+    from: 'New-Ticket <itsupport@objectways.com>',
+    to: 'itsupport@objectways.com',
+    cc: 'parthiban@objectways.com',
+    subject: `New Ticket from the employee ID ${req.body.employeeId}`,
+    text: `Name: ${req.body.employeeName}\nEmail: ${req.body.emailId}\nMessage: ${req.body.description}`, // plain text body
+    html: `<p>Name: ${req.body.employeeName}</p><p>Email: ${req.body.emailId}</p><p>Issue: ${req.body.systemType}</p><p>Message: ${req.body.description}</p>` // html body
+  }
+  transpoter.sendMail(mailOptions, function (error, info) {
+    if (err) {
+      console.log(error);
+    } else {
+      res.redirect('/');
+      console.log('email sent' + info.response)
+    }
+
+  })
+
+});
+
+// Add a new review
+app.post('/api/timechamp', (req, res) => {
+  const { employeeIdTwo, systemNoTwo, systemTypeTwo, emailID, unitNoTwo, floorNoTwo, teamNameTwo, priorityTwo, issueDateTwo, descriptionTwo, } = req.body;
+
+  const newReviewTwo = new ReviewTwo({
+   employeeIdTwo, systemNoTwo, systemTypeTwo, emailID, unitNoTwo, floorNoTwo, teamNameTwo, priorityTwo, issueDateTwo, descriptionTwo
+  });
+
+  newReviewTwo.save()
+    .then(review => {
+      res.send(review);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Error saving review');
+    });
+
+  // nodemailer function
+  const transpoter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'parthibaneee7548@gmail.com',
+      pass: 'xnzrszhaawvpkcov'
+    }
+  })
+  const mailOptions = {
+    from: 'New-Ticket <it-support@example.com>',
+    to: 'parthiban@objectways.com',
+    cc: 'parthibaneee7548@gmail.com',
+    subject: `New Ticket from the team ${req.body.systemTypeTwo}`,
+    text: `Team Name: ${req.body.systemTypeTwo}\nTeam Manager: ${req.body.systemNoTwo}\nMessage: ${req.body.descriptionTwo}`, // plain text body
+    html: `<p>Team Name: ${req.body.systemTypeTwo}</p><p>Team Manager: ${req.body.systemNoTwo}</p><p>Issue: ${req.body.priorityTwo}</p><p>Message: ${req.body.descriptionTwo}</p>` // html body
+  }
+  transpoter.sendMail(mailOptions, function (error, info) {
+    if (err) {
+      console.log(error);
+    } else {
+      res.redirect('/');
+      console.log('email sent' + info.response)
+    }
+
+  })
+
+});
+
+// Add a new review
+app.post('/api/hrreview', (req, res) => {
+
+
+  const { employeeNameThree, employeeIdThree, systemNoThree, systemTypeThree, floorNoThree, unitNoThree, teamNameThree, teamManagerThree, priorityThree, issueDateThree, descriptionThree } = req.body;
+
+  const newReviewThree = new ReviewThree({
+    employeeNameThree, employeeIdThree, systemNoThree, systemTypeThree, floorNoThree, unitNoThree, teamNameThree, teamManagerThree, priorityThree, issueDateThree, descriptionThree
+  });
+
+  newReviewThree.save()
+    .then(reviewthree => {
+      res.send(reviewthree);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Error saving review');
+    });
+
+  // nodemailer function
+  const transpoter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'parthibaneee7548@gmail.com',
+
+      pass: 'xnzrszhaawvpkcov'
+    }
+  })
+  const mailOptions = {
+    from: 'New-Ticket <it-support@example.com>',
+    to: 'parthiban@objectways.com',
+    cc: 'parthibaneee7548@gmail.com',
+    subject: `New Ticket from the employee ID ${req.body.employeeNameThree}`,
+    text: `Name: ${req.body.systemNoThree}\nEmail: ${req.body.employeeIdThree}\nMessage: ${req.body.descriptionThree}`, // plain text body
+    html: `<p>Name: ${req.body.systemNoThree}</p><p>Email: ${req.body.employeeIdThree}</p><p>Issue: ${req.body.systemTypeThree}</p><p>Message: ${req.body.descriptionTwo}</p>` // html body
+  }
+  transpoter.sendMail(mailOptions, function (error, info) {
+    if (err) {
+      console.log(error);
+    } else {
+      res.redirect('/');
+      console.log('email sent' + info.response)
+    }
+
+  })
+
+});
+
+
+// Get all reviews
+app.get('/api/reviews', async (req, res) => {
   try {
-    const { name, address, experience, previousCompany, noticePeriod, lastCTC, expectedCTC, phoneNO } = req.body;
-
-    // Handle form submission logic here
-    // You can access the uploaded resume file using req.file
-
-    // Example logic: Print form data and file details
-    console.log('Form Data:');
-    console.log('Name:', name);
-    console.log('Address:', address);
-    console.log('Experience:', experience);
-    console.log('Previous Company:', previousCompany);
-    console.log('Notice Period:', noticePeriod);
-    console.log('Last CTC:', lastCTC);
-    console.log('Expected CTC:', expectedCTC);
-    console.log('Phone No:', phoneNO);
-
-    if (req.file) {
-      console.log('Uploaded Resume:');
-      console.log('File Name:', req.file.originalname);
-      console.log('File Path:', req.file.path);
-    }
-
-    // Configure your email provider details here (e.g., SMTP)
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'parthibaneee7548@gmail.com',
-        pass: 'xnzrszhaawvpkcov'
-      }
-    });
-
-    // Send email using nodemailer
-    const mailOptions = {
-      from: 'New Application', // Replace with your email address
-      to: 'balamuruganveerappan@objectways.com', // Replace with the recipient email address
-      cc: 'parthiban@objectways.com',
-      subject: 'Job Application',
-      text: `A new job application has been submitted.\n\nName: ${name}\nAddress: ${address}\nExperience: ${experience}\nPrevious Company: ${previousCompany}\nNotice Period: ${noticePeriod}\nLast CTC: ${lastCTC}\nExpected CTC: ${expectedCTC}\nPhone No: ${phoneNO}`,
-      attachments: []
-    };
-
-    if (req.file) {
-      mailOptions.attachments.push({ filename: req.file.originalname, path: req.file.path });
-    }
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Error occurred while sending email' });
-      } else {
-        console.log('Email sent: ' + info.response);
-        res.status(200).json({ success: true, message: 'Application submitted successfully' });
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    const reviews = await Review.find({});
+    res.send(reviews);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error getting reviews');
   }
 });
+
+// Get all reviews
+app.get('/api/timechamp', async (req, res) => {
+  try {
+    const reviewstwo = await ReviewTwo.find({});
+    res.send(reviewstwo);
+    res.send(timechamp);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error getting reviews');
+  }
+});
+
+// Get all reviews
+app.get('/api/hrreview', async (req, res) => {
+  try {
+    const reviewsthree = await ReviewThree.find({});
+    res.send(reviewsthree);
+    res.send(hrreview);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error getting reviews');
+  }
+});
+
+// Delete a review
+app.delete('/api/reviews/:id', async (req, res) => {
+  try {
+    const review = await Review.findByIdAndDelete(req.params.id);
+    if (!review) {
+      res.status(404).send('Review not found');
+    } else {
+      res.send(review);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error deleting review');
+  }
+});
+
+// Delete a review
+app.delete('/api/timechamp/:id', async (req, res) => {
+  try {
+    const reviewtwo = await ReviewTwo.findByIdAndDelete(req.params.id);
+    if (!reviewtwo) {
+      res.status(404).send('Review not found');
+    } else {
+      res.send(reviewtwo);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error deleting review');
+  }
+});
+
+// Delete a review
+app.delete('/api/hrreview/:id', async (req, res) => {
+  try {
+    const reviewthree = await ReviewThree.findByIdAndDelete(req.params.id);
+    if (!reviewthree) {
+      res.status(404).send('Review not found');
+    } else {
+      res.send(reviewthree);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error deleting review');
+  }
+});
+
+
+// IT Admin Dashboard
+// define API routes
+app.post('/api/accept', (req, res) => {
+  // update the ticket status to "accepted"
+  const ticket = req.body;
+  ticket.status = 'accepted';
+
+  // TODO: update the ticket status in the database
+
+  // return success response
+  res.status(200).send('Ticket accepted successfully');
+});
+
+app.post('/api/resolve', (req, res) => {
+  // update the ticket status to "resolved"
+  const ticket = req.body;
+  ticket.status = 'resolved';
+
+  // TODO: update the ticket status in the database
+
+  // return success response
+  res.status(200).send('Ticket resolved successfully');
+});
+
+app.post('/api/send-email', (req, res) => {
+  // send email to user using nodemailer
+  const emailData = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'parthibaneee7548@gmail.com',
+
+      pass: 'xnzrszhaawvpkcov'
+    }
+  });
+
+  const mailOptions = {
+    from: 'IT-Support <itsupport@objectways.com>', // replace with your Gmail address
+    to: emailData.to,
+    subject: emailData.subject,
+    text: emailData.text
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('There was an error sending the email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+
+// Hr dashboard
+// define API routes
+app.post('/api/accepthr', (req, res) => {
+  // update the ticket status to "accepted"
+  const ticket = req.body;
+  ticket.status = 'accepted';
+
+  // TODO: update the ticket status in the database
+
+  // return success response
+  res.status(200).send('Ticket accepted successfully');
+});
+
+app.post('/api/resolvehr', (req, res) => {
+  // update the ticket status to "resolved"
+  const ticket = req.body;
+  ticket.status = 'resolved';
+
+  // TODO: update the ticket status in the database
+
+  // return success response
+  res.status(200).send('Ticket resolved successfully');
+});
+
+app.post('/api/send-emailhr', (req, res) => {
+  // send email to user using nodemailer
+  const emailData = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'parthibaneee7548@gmail.com',
+
+      pass: 'xnzrszhaawvpkcov'
+    }
+  });
+
+  const mailOptions = {
+    from: 'HRM <hrm@objectways.com>', // replace with your Gmail address
+    to: emailData.to,
+    subject: emailData.subject,
+    text: emailData.text
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('There was an error sending the email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+// Timechamp Dashboard
+// define API routes
+app.post('/api/accepttc', (req, res) => {
+  // update the ticket status to "accepted"
+  const ticket = req.body;
+  ticket.status = 'accepted';
+
+  // TODO: update the ticket status in the database
+
+  // return success response
+  res.status(200).send('Ticket accepted successfully');
+});
+
+app.post('/api/resolvetc', (req, res) => {
+  // update the ticket status to "resolved"
+  const ticket = req.body;
+  ticket.status = 'resolved';
+
+  // TODO: update the ticket status in the database
+
+  // return success response
+  res.status(200).send('Ticket resolved successfully');
+});
+
+app.post('/api/send-emailtc', (req, res) => {
+  // send email to user using nodemailer
+  const emailData = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'parthibaneee7548@gmail.com',
+
+      pass: 'xnzrszhaawvpkcov'
+    }
+  });
+
+  const mailOptions = {
+    from: 'Facility-Support <loganathanvenkatesh@objectways.com>', // replace with your Gmail address
+    to: emailData.to,
+    subject: emailData.subject,
+    text: emailData.text
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('There was an error sending the email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
 
 // static files
 app.use(express.static(path.join(__dirname, './client/build')))
@@ -89,8 +432,9 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
-// Start the server
-const port = process.env.PORT || 5000
+
+// port
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
